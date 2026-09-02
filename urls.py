d369811228bbb8cc97b1287173f820907ea94354
priyadharshinikumar369-rgb <priyadharshinikumar369@gmail.com>
2026-09-02 20:16:15 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,35 +1,33 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for config project.
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-
-    # Inventory List
-    path(
-        '',
-        views.inventory_list,
-        name='inventory_list'
-    ),
-
-    # Add Inventory
-    path(
-        'add/',
-        views.inventory_add,
-        name='inventory_add'
-    ),
-
-    # Edit Inventory
-    path(
-        'edit/<int:inventory_id>/',
-        views.inventory_edit,
-        name='inventory_edit'
-    ),
-
-    # Delete Inventory
-    path(
-        'delete/<int:inventory_id>/',
-        views.inventory_delete,
-        name='inventory_delete'
-    ),
-
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('inventory/', include('inventory.urls')),
+    path('donor/', include('donor.urls')),
+    path('patient/', include('patient.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
